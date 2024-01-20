04689d2ccde21165769400580ac468d80a3bc42a
BỊP Device <152129878+bipdevice@users.noreply.github.com>
2024-01-20 17:17:57 +0700

    Create connect-wifi.js

---
+++ utils/connect-wifi.js
@@ -0,0 +1,15 @@
+(async function(){
+    const tenWifi = "Ten_Wifi";
+    const matkhauWifi = "PasswordWifi";
+  
+    await ExecAdbCmd("shell", "am", "start", "-a", "android.settings.SETTINGS");
+    await Delay(1000);
+    await TapBySelector(`[text="Network & internet"]`);
+    await Delay(1000);
+    await TapBySelector(`[text="Not connected"]`);
+    await Delay(3000);
+    await TapBySelector(`[text="${tenWifi}"]`);
+    await Delay(2000);
+    await SendText(matkhauWifi);
+    await SendKeyEvent("KEYCODE_ENTER");
+})();
